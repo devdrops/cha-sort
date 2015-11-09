87a41bc4d5575efd3b21ea4d3a6c618bb23318e2
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-11-08 22:04:22 -0200

    Changens for database usage

--- app/config/prod.php
+++ app/config/prod.php
@@ -9,10 +9,11 @@ $app['cache.path'] = __DIR__ . '/../cache';
 $app['twig.options.cache'] = $app['cache.path'] . '/twig';
 
 $app['db.options'] = [
-    'driver'   => 'database_driver',
-    'host'     => 'database_host',
-    'port'     => 'database_port',
-    'dbname'   => 'database_name',
-    'user'     => 'database_user',
-    'password' => 'database_password'
+    'driver'   => 'pdo_mysql',
+    'host'     => OPENSHIFT_MYSQL_DB_HOST,
+    'port'     => OPENSHIFT_MYSQL_DB_PORT,
+    'dbname'   => 'chasort',
+    'user'     => OPENSHIFT_MYSQL_DB_USERNAME,
+    'password' => OPENSHIFT_MYSQL_DB_PASSWORD,
+    'charset'  => 'UTF8'
 ];
